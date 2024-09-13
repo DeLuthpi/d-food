a87668bed40a89be43fc66f699bf849ec7939e67
DeLuthpi <dedeluthpi006@gmail.com>
2024-09-13 21:38:49 +0700

    add food list display

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,63 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { getCookie } from "cookies-next";
+import Link from "next/link";
+
 const HomePage = () => {
+	const token = getCookie("token");
+
+	const [data, setData] = useState([]);
+	const [loading, setLoading] = useState(false);
+
+	const getData = () => {
+		setLoading(true);
+		axios
+			.get("https://api-bootcamp.do.dibimbing.id/api/v1/foods", {
+				headers: {
+					apiKey: "w05KkI9AWhKxzvPFtXotUva-",
+					Authorization: `Bearer ${token}`,
+				},
+			})
+			.then((res) => {
+				setData(res.data.data);
+				setLoading(false);
+			})
+			.catch((err) => {
+				console.log(err);
+			})
+			.finally(() => {
+				setLoading(false);
+			});
+	};
+
+	useEffect(() => {
+		getData();
+	}, []);
+
+	if (loading) return <div>Loading...</div>;
+
 	return (
-		<div className="flex flex-col items-center justify-between min-h-screen p-24">
-			<h1 className="text-2xl">Ini Halaman Home</h1>
-		</div>
+		<main className="main-section">
+			<h1 className="mt-10 text-2xl text-center">D&apos;Food - Menu List</h1>
+			<hr className="line-hr" />
+			<div className="grid grid-cols-1 mx-6 mt-6 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+				{data.map((item) => (
+					<div className="relative group" key={item?.id}>
+						<div className="w-full overflow-hidden bg-gray-200 rounded-t-md aspect-h-1 aspect-w-1 lg:aspect-none group-hover:opacity-75 lg:h-80">
+							<img src={item?.imageUrl} alt={item?.name} className="object-cover object-center w-full h-[250px] text-gray-900 lg:h-full lg:w-full" />
+						</div>
+						<div className="flex justify-center p-4 bg-white rounded-b-md">
+							<h3 className="text-base font-semibold text-gray-900">
+								<Link href={`/${item?.id}`}>
+									<span aria-hidden="true" className="absolute inset-0"></span>
+									{item?.name}
+								</Link>
+							</h3>
+						</div>
+					</div>
+				))}
+			</div>
+		</main>
 	);
 };
 
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -41,7 +41,7 @@ const LoginPage = () => {
 			<div className="flex flex-col justify-center min-h-full px-6 py-12 h-svh lg:px-8">
 				<div className="p-5 mx-auto border border-gray-300 border-solid rounded-lg w-full md:w-96 bg-gradient-to-br from-[#242118] to-[#242118]">
 					<div className="sm:mx-auto sm:w-full sm:max-w-sm">
-						<h1 className="text-2xl font-bold leading-9 tracking-tight text-center">D - Food</h1>
+						<h1 className="text-2xl font-bold leading-9 tracking-tight text-center">D&apos;Food</h1>
 						<hr className="line-hr" />
 						<h2 className="mt-4 text-lg font-normal leading-9 tracking-tight text-center">Sign in to your account</h2>
 					</div>
